7215cb3f94bd0ba49739488e5462ccbca35c8cd1
unknown <murraymclellan@hotmail.co.uk>
2024-01-21 17:44:51 +0700

    Adding registration

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ console.log(`${x.user.tag} is ready!`);
 
 client.on ("interactionCreate", async (interaction) => {
     if(interaction.isCommand()) {
+        //Group finder interaction
         if(interaction.commandName === "lfg") {
             const username = interaction.user.name;
             const userId = interaction.user.id;
             const userTag = client.users.cache.get(userId);
             console.log(`User ${username} used the LFG command.`);
             const gameReceived = interaction.options.getString("game");
-            const nopReceived = interaction.options.getInteger("players")
+            const nopReceived = interaction.options.getInteger("players");
             const groupFinder = 'group-finder';
             const targetChannel = interaction.guild.channels.cache.find((channel) => channel.name === groupFinder);
             interaction.reply({content: `You said, you want to find a group of ${nopReceived} for ${gameReceived}. Your message will be posted in the Group Finder channel.`, ephemeral: true})
@@ -28,8 +29,27 @@ client.on ("interactionCreate", async (interaction) => {
                 console.log(`Channel '${targetChannel}' not found.` );
             }
         }
+        //Event interaction
+        if(interaction.commandName === 'eventreg') {
+            const userId = interaction.user.id;
+            const userTag = client.users.cache.get(userId);
+            const eventJoined = interaction.options.getString("event");
+            const registeredTeam = interaction.options.getString("eventgroup");
+            const teamName = interaction.options.getString("teamname");
+            const echannelAdmin = interaction.guild.channels.cache.find((channel) => channel.name === 'event-teams');;
+            const eventChannel = interaction.guild.channels.cache.find((channel) => channel.name === 'weekly-events-discussion');
+            console.log(`User ${userTag} registered for the event.`)
+            interaction.reply({content: `You registered ${teamName} for the ${eventJoined} event with your group consisting of ${registeredTeam}.`, ephemeral: true});
+            if(eventChannel){
+                echannelAdmin.send(`${userTag} has registered for the ${eventJoined} event with ${teamName} consisting of the players: ${registeredTeam}. `)
+                eventChannel.send(`${teamName} have registered for the ${eventJoined} event. Best of luck!`)
+            }else{
+                console.log(`Channel '${eventChannel}' not found.` );
+            }
+        }
     }
 })
+
 
 
 
--- slash-deploy.js
+++ slash-deploy.js
@@ -4,6 +4,7 @@ const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 //Info needed for slash commands
 const botId = "1195077330939416648"
 const serverId = "872734285398937620";
+//const serverId = '949625437045276743';
 const botToken = process.env.TOKEN;
 
 const rest = new REST().setToken(botToken);
@@ -11,29 +12,46 @@ const rest = new REST().setToken(botToken);
 
 const slashRegister = async () => {
     try {
-        await rest.put(Routes.applicationGuildCommands(botId,serverId), {
+        await rest.put(Routes.applicationGuildCommands(botId, serverId), {
             body: [
                 new SlashCommandBuilder()
-                .setName("lfg")
-                .setDescription("Use this command to find people to play with.")
-                .addStringOption( option => {
-                    return option
-                    .setName("game")
-                    .setDescription("Write the name of your game here.")
-                    .setRequired(true)
-                })
-                .addIntegerOption(option => {
-                    return option
-                    .setName("players")
-                    .setDescription("How many players do you want to find?")
-                    .setRequired(true);
-                })
+                    .setName("lfg")
+                    .setDescription("Use this command to find people to play with.")
+                    .addStringOption(option => option
+                        .setName("game")
+                        .setDescription("Write the name of your game here.")
+                        .setRequired(true)
+                    )
+                    .addIntegerOption(option => option
+                        .setName("players")
+                        .setDescription("How many players do you want to find?")
+                        .setRequired(true)
+                    ),
+
+                new SlashCommandBuilder()
+                    .setName("eventreg")
+                    .setDescription("Register your group for the event.")
+                    .addStringOption(option => option
+                        .setName("event")
+                        .setDescription("Write the name of the event here.")
+                        .setRequired(true)
+                    )
+                    .addStringOption(option => option
+                        .setName("eventgroup")
+                        .setDescription("Write the username of each team member separate by commas.")
+                        .setRequired(true)
+                    )
+                    .addStringOption(option => option
+                        .setName("teamname")
+                        .setDescription("Choose a team name.")
+                        .setRequired(true) 
+                    )
             ]
-        })
+        });
     } catch (error) {
         console.error(error);
     }
-}
+};
 
 slashRegister();
 
